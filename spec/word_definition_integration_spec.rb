042ab38ff9af3d2887da6a0055a820266651156d
Janice <janicedunn53@gmail.com>
2015-08-14 13:31:11 -0700

    Allow user to add a word to the word list.

---
+++ spec/word_definition_integration_spec.rb
@@ -0,0 +1,39 @@
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe('path to view all words', {:type => :feature}) do
+  it('sends the user to the word list') do
+    visit('/')
+    click_link("See Word List")
+    expect(page).to have_content("Words")
+  end
+end
+
+describe('path to add new word', {:type => :feature}) do
+  it('sends user to a form to add a word') do
+    visit('/')
+    click_link("Add New Word")
+    expect(page).to have_content("Add words to your dictionary")
+  end
+end
+
+describe('path to words form', {:type => :feature}) do
+  it('processes user entry and adds a word') do
+    visit('/words/new')
+    fill_in('name', :with => "simple")
+    click_button("Add Word")
+    expect(page).to have_content("Success")
+  end
+end
+
+describe('path to all words', {:type => :feature}) do
+  it('sends user to word list') do
+    visit('/words/new')
+    fill_in('name', :with => "complex")
+    click_button("Add Word")
+    click_link("See Word List")
+    expect(page).to have_content("Words")
+  end
+end
